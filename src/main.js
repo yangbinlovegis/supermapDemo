c721fa67d193dff916c9d1fd6806995d4034382b
杨斌(10027866) <yangb-g@glodon.com>
2020-12-21 09:33:36 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import '@supermap/vue-iclient3d-webgl/dist/styles/vue-iclient3d-webgl.min.css'
 import VueiClientWebgl from '@supermap/vue-iclient3d-webgl'
 
-Vue.use(VueiClientWebgl);
+Vue.use(VueiClientWebgl)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
